0be277aacd5229cfcb90b46461b7d1aba76ba3a8
Juan José Fernández <juanjofer1999@gmail.com>
2020-03-06 22:11:54 -0400

    algo

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:app_grafos/grafo.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp()
+    )
+  );
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyApp();
+  }
+
+}
+
+class _MyApp extends State<MyApp> {
+  Grafo game;
+  void Start()async{
+    WidgetsFlutterBinding.ensureInitialized();
+    Util flameUtil = Util();
+
+    game = Grafo(context);
+
+    await flameUtil.fullScreen();
+    await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+    
+    TapGestureRecognizer tapper = TapGestureRecognizer();
+    tapper.onTapDown = game.onTapDown;
+    flameUtil.addGestureRecognizer(tapper);
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: game.widget!=null?game.widget:Container());
+  }
+  void initState() {
+    Start();
+  }
+}
---
+++ nodo.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:app_grafos/grafo.dart';
+
+class Nodo {
+  final Grafo game;
+  Size screenSize;
+  Offset circle,offSetText;
+  Paint paint;
+  Rect pos;
+  TextSpan textSp;
+  TextPainter textPaint;
+
+  Nodo(this.game,double x,double y,String text){
+    textSp=new TextSpan(text: text,style: TextStyle(color: Colors.black,fontSize: game.tileSize/2));
+    textPaint=new TextPainter(text:textSp,textAlign:TextAlign.center,textDirection:TextDirection.rtl);
+    textPaint.layout();
+    pos=Rect.fromLTWH(x-game.tileSize/2, y-game.tileSize/2, game.tileSize, game.tileSize);
+    paint=new Paint();
+    paint.color=Color(0xffdd3d16);
+    circle = Offset(x, y);
+  }
+  void render(Canvas canvas) {
+    canvas.drawCircle(circle, game.tileSize/2,paint);
+    double textW=textPaint.width,textH=textPaint.height;   
+    offSetText=Offset(circle.dx-textW/2, circle.dy-textH/2);
+    textPaint.paint(canvas,offSetText);
+  }
+
+  void selectNodo(){
+    paint.color=Color(0xffc4710d);  
+  }
+  void update(double t) {
+    
+  }
+
+}
